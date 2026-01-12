29ba8f1bb69360318198274b451bb22b9ceb781c
Agustin Farisano <afarisano@bitbucket.org>
2026-01-12 16:19:34 -0300

    solucion de ejercicio de form de heroe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Counter } from './pages/counter/counter';
+import { HeroPageComponent } from './pages/hero-page-component/hero-page-component';
 
 export const routes: Routes = [
   {
     path: '',
     component: Counter
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent
   }
 ];
