834bc6956164583e30dd956f4a0b66805fd4d1c9
Agustin Farisano <afarisano@bitbucket.org>
2026-01-14 09:36:32 -0300

    agrego condicional para mostrar personajes segun su poder

--- src/app/pages/dragonball-page/dragonball-page.ts
+++ src/app/pages/dragonball-page/dragonball-page.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 
 interface Character {
@@ -19,7 +20,15 @@ export class DragonballPage {
     characters = signal<Character[]>([
       {id: 1, name: 'Goku', power: 9001},
       {id:2, name: 'Vegeta', power: 5000},
-      {id:3, name: 'Piccolo', power:7000}
+      {id:3, name: 'Piccolo', power:7000},
+      {id:4, name: 'Yamcha', power:500}
     ]);
+
+
+    powerClasses = computed(()=>{
+         return{
+            'text-danger':true,
+         };
+    });
 
 }
